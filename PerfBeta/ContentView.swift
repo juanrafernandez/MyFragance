e9d2e12a3eb89c2a23ce46f211629be88dc3b76a
Juan Ramon Fernandez Calvo <juan-ramon.fernandez@prosegur.com>
2024-11-26 23:25:28 +0100

    Initial Commit

---
+++ PerfBeta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PerfBeta
+//
+//  Created by ES00571759 on 26/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PerfBeta/PerfBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfBetaApp.swift
+//  PerfBeta
+//
+//  Created by ES00571759 on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PerfBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
